aa67762e190eb684fe4327d2f622563a82631864
Justin Acheaibi <justin.acheaibi@gmail.com>
2025-10-31 06:44:55 +0100

    Fix: align portraits.scss

--- src/pages/Portraits.jsx
+++ src/pages/Portraits.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import '../styles/Portraits.scss';
+import '../styles/portraits.scss';
 import AvisGoogle from '../components/AvisGoogle';
 
 function Portraits() {
